9c7080a524fa39f3ebef520e925fcf04f371c294
Rahul Sharma <rahulsharma@C02DW8ANMD6M.local>
2022-01-21 16:09:34 +0530

    Added default feeds in seed file.

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+Feed.create([
+  {
+    url: 'https://timesofindia.indiatimes.com/rssfeeds/-2128936835.cms',
+    interval_seconds: 61,
+    should_fetch: true
+  },
+  {
+    url: 'https://www.thehindu.com/news/national/feeder/default.rss',
+    interval_seconds: 61,
+    should_fetch: true
+  }
+])
